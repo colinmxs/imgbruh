b3f536b13a05803e0f35434401072170238b3886
colinmxs <smith.colin00@gmail.com>
2017-03-14 10:53:59 -0600

    wip

--- imgbruh/Features/Imgs/ImgsController.cs
+++ imgbruh/Features/Imgs/ImgsController.cs
@@ -51,7 +51,6 @@ namespace imgbruh.Features.Imgs
             return this.RedirectToActionJson(nameof(DetailsAsync), new { codeName = command.Name });
         }        
 
-        // GET: Imgs/Details/5
         [Route("{codename}")]
         public async Task<ActionResult> DetailsAsync(string codeName)
         {
@@ -63,66 +62,5 @@ namespace imgbruh.Features.Imgs
             var img = await _mediator.SendAsync(query);
             return View(img);
         }
-
-
-
-        //// GET: Imgs/Edit/5
-        //public async Task<ActionResult> Edit(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    Img img = await db.Imgs.FindAsync(id);
-        //    if (img == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    ViewBag.UserId = new SelectList(db.Users, "Id", "Email", img.UserId);
-        //    return View(img);
-        //}
-
-        //// POST: Imgs/Edit/5
-        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<ActionResult> Edit([Bind(Include = "Id,CodeName,Url,TimeCreatedUtc,UserId")] Img img)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        db.Entry(img).State = EntityState.Modified;
-        //        await db.SaveChangesAsync();
-        //        return RedirectToAction("Index");
-        //    }
-        //    ViewBag.UserId = new SelectList(db.Users, "Id", "Email", img.UserId);
-        //    return View(img);
-        //}
-
-        //// GET: Imgs/Delete/5
-        //public async Task<ActionResult> Delete(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    Img img = await db.Imgs.FindAsync(id);
-        //    if (img == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    return View(img);
-        //}
-
-        //// POST: Imgs/Delete/5
-        //[HttpPost, ActionName("Delete")]
-        //[ValidateAntiForgeryToken]
-        //public async Task<ActionResult> DeleteConfirmed(int id)
-        //{
-        //    Img img = await db.Imgs.FindAsync(id);
-        //    db.Imgs.Remove(img);
-        //    await db.SaveChangesAsync();
-        //    return RedirectToAction("Index");
-        //}
     }
 }
--- imgbruh/Infrastructure/FileStorage.cs
+++ imgbruh/Infrastructure/FileStorage.cs
@@ -2,45 +2,38 @@
 {
     using Microsoft.WindowsAzure.Storage;
     using Microsoft.WindowsAzure.Storage.Auth;
-    using System;
     using System.Configuration;
     using System.IO;
     using System.Threading.Tasks;
-    using System.Web;
 
     public class FileStorage
     {
-        private const string ResourceName = "imgbruh";
+        private readonly string ResourceName = ConfigurationManager.AppSettings["imgbruh.blob.core:resourceName"];
+        //private const string ResourceName = "imgbruh";
         private const string ProtocolPrefix = "https://";
         private readonly string EndpointSuffix = ConfigurationManager.AppSettings["imgbruh.blob.core:endpointSuffix"];
         private readonly string Key1 = ConfigurationManager.AppSettings["imgbruh.blob.core:key1"];
         private readonly string Key2 = ConfigurationManager.AppSettings["imgbruh.blob.core:key2"];
         private readonly string DefaultContainer = ConfigurationManager.AppSettings["imgbruh.blob.core:defaultContainer"];
 
-        public async Task<string> UploadBlobAsync(Stream fileStream, string name)
+        public async Task UploadBlobAsync(Stream fileStream, string name)
         {
             //todo: refactor to get rid of this
-#if DEBUG
-            var localStorageConnectionString = ConfigurationManager.AppSettings["localStorageConnection"];
-            var storageAccount = CloudStorageAccount.Parse(localStorageConnectionString);
-            var storageClient = storageAccount.CreateCloudBlobClient();
-            var defaultContainer = storageClient.GetContainerReference(ConfigurationManager.AppSettings["localStorageDefaultContainer"]);
-            var blob = defaultContainer.GetBlockBlobReference(name);
-            var url = "http://127.0.0.1:10000/devstoreaccount1/default/" + name;
-#else
+            //var localStorageConnectionString = ConfigurationManager.AppSettings["localStorageConnection"];
+            //var storageAccount = CloudStorageAccount.Parse(localStorageConnectionString);
+            //var url = "http://127.0.0.1:10000/devstoreaccount1/default/" + name;
+
+
             var storageCredentials = new StorageCredentials(ResourceName, Key1);
             var storageAccount = new CloudStorageAccount(storageCredentials, null, true);
             var storageClient = storageAccount.CreateCloudBlobClient();
             var defaultContainer = storageClient.GetContainerReference(DefaultContainer);
             var blob = defaultContainer.GetBlockBlobReference(name);
             var url = ProtocolPrefix + ResourceName + EndpointSuffix + "/" + DefaultContainer + "/"+ name;
-#endif
             using (fileStream)
             {
                 await blob.UploadFromStreamAsync(fileStream);
             }
-
-            return url;
         }
     }    
 }
